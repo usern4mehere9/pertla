f36e2b82346afe8c6f7ca19d88468a023b26a838
usern4mehere9 <91462628+usern4mehere9@users.noreply.github.com>
2021-10-06 12:52:36 +0200

    Add files via upload

---
+++ pertla/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace pertla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n, m, ok, bp;
+            n = double.Parse(Console.ReadLine());
+            m = double.Parse(Console.ReadLine());
+
+            bp = Math.Truncate(n / m);
+            ok = n - (m * bp);
+
+            Console.WriteLine(bp);
+            Console.WriteLine(ok);
+        }
+    }
+}
